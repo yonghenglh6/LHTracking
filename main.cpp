20d2012d0957be790830eb1ad68347e66470d3c2
刘灏@apollo <haoliu@deepglint.com>
2018-02-11 19:16:08 +0800

    Revert "[add] BaseClass Track.h & namespace"

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@
 
 using namespace std;
 using namespace cv;
-using namespace Tracking4_0;
 
 const unsigned char LabelColors[][3] = {{251, 144, 17},
                                         {2,   224, 17},
